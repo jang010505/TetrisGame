90b6e2bfa823f8b9a22969d71880bd4ec05ed27e
hg_yellow <jang010505@gmail.com>
2021-02-17 00:43:39 +0900

    .

--- TetrisGame.c
+++ TetrisGame.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <math.h>
 #include <time.h>
 #include <conio.h>
 #include <windows.h>
@@ -12,7 +11,19 @@
 #define DOWN 80     // 반시계방향 회전
 #define SPACEBAR 32 // 고속 낙하
 
-int blocks[7][4][4][4] = {
+clock_t startDropT, endT, startGroundT;
+int x = 8, y = 0; // 블럭의 위치
+RECT blockSize;
+int blockNum;        // 블럭 모양 0~6
+int blockVector = 0; // 블럭 방향
+int key;
+
+void init();
+void move(int x, int y);
+void randomblock();
+int Check(int x, int y);
+
+int block[7][4][4][4] = {
     // block[도형의 종류][도형의 방향]
     // block1
     {{{0, 0, 0, 0}, {0, 1, 0, 0}, {0, 1, 1, 1}, {0, 0, 0, 0}}, {{0, 0, 0, 0}, {0, 1, 1, 0}, {0, 1, 0, 0}, {0, 1, 0, 0}}, {{0, 0, 0, 0}, {1, 1, 1, 0}, {0, 0, 1, 0}, {0, 0, 0, 0}}, {{0, 0, 1, 0}, {0, 0, 1, 0}, {0, 1, 1, 0}, {0, 0, 0, 0}}},
@@ -28,7 +39,7 @@ int blocks[7][4][4][4] = {
     {{{0, 0, 0, 0}, {0, 1, 1, 0}, {0, 0, 1, 1}, {0, 0, 0, 0}}, {{0, 0, 0, 0}, {0, 0, 1, 0}, {0, 1, 1, 0}, {0, 1, 0, 0}}, {{0, 0, 0, 0}, {1, 1, 0, 0}, {0, 1, 1, 0}, {0, 0, 0, 0}}, {{0, 0, 1, 0}, {0, 1, 1, 0}, {0, 1, 0, 0}, {0, 0, 0, 0}}},
     // block7
     {{{0, 0, 0, 0}, {0, 1, 1, 0}, {1, 1, 0, 0}, {0, 0, 0, 0}}, {{0, 1, 0, 0}, {0, 1, 1, 0}, {0, 0, 1, 0}, {0, 0, 0, 0}}, {{0, 0, 0, 0}, {0, 0, 1, 1}, {0, 1, 1, 0}, {0, 0, 0, 0}}, {{0, 0, 0, 0}, {0, 1, 0, 0}, {0, 1, 1, 0}, {0, 0, 1, 0}}}};
-int space[TABLE_Y][TABLE_Y] = {
+int table[TABLE_Y][TABLE_Y] = { // 테이블
     {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
     {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
     {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
@@ -52,5 +63,53 @@ int space[TABLE_Y][TABLE_Y] = {
     {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1}};
 int main()
 {
-    int n;
+    init();
+}
+void init()
+{
+    CONSOLE_CURSOR_INFO cursorinfo;
+    cursorinfo.bVisible = 0;
+    cursorinfo.dwSize = 1;
+    SetConsoleCursorInfo(GetStdHandle(STD_OUTPUT_HANDLE), &cursorinfo);
+    srand(time(NULL));
+}
+void move(int x, int y)
+{
+    COORD p;
+    p.X = x;
+    p.Y = y;
+    SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), p);
+}
+void randomblock()
+{
+    blockNum = rand() % 7;
+}
+int Check(int x, int y)
+{
+    for (int i = 0; i < 4; i++)
+    {
+        for (int j = 0; j < 4; j++)
+        {
+            if (block[blockNum][blockVector][i][j] == 1)
+            {
+                int chk = table[i + y][j + x / 2];
+                if (chk == 1 || chk == 2) // 벽일 때, 블럭일 때
+                    return 1;
+            }
+        }
+    }
+    return 0;
+}
+void dropBlock()
+{
+    endT = clock();
+    if ((float)(endT - startDropT) >= 800)
+    {
+        if (Check(x, y + 1))
+            return;
+        y++;
+        startDropT = clock();
+        startGroundT = clock();
+        system("cls");
+    }
 }
