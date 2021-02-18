6fe3ef0f7fa3205917534cb9b052db7641a07bba
hg_yellow <jang010505@gmail.com>
2021-02-19 01:36:57 +0900

    updata color

--- TetrisGame_Py.py
+++ TetrisGame_Py.py
@@ -13,26 +13,35 @@ height = 21
 sqauresize = 25
 
 # color
-BLACK = (0, 0, 0)
-WHITE = (255, 255, 255)
-PINK = (255, 178, 217)
-PURPLE = (209, 178, 255)
-SKYBLUE = (178, 235, 244)
-LIGHTGREEN = (183, 240, 177)
-YELLOW = (250, 237, 125)
-ORANGE = (255, 193, 158)
-RED = (255, 167, 167)
+color = {
+    "BLACK": (0, 0, 0),
+    "WHITE": (255, 255, 255),
+    "PINK": (255, 178, 217),
+    "PURPLE": (209, 178, 255),
+    "SKYBLUE": (178, 235, 244),
+    "LIGHTGREEN": (183, 240, 177),
+    "YELLOW": (250, 237, 125),
+    "ORANGE": (255, 193, 158),
+    "RED": (255, 167, 167)}
+
+#table, block
+table = [[0]*width for i in range(height)]
+for i in range(height):
+    table[i][0] = 1
+    table[i][11] = 1
+for i in range(width):
+    table[20][i] = 1
 
 
 def drawmain(self, index, check):
-    self.fill(SKYBLUE)
+    self.fill(color["SKYBLUE"])
     fontmain = pygame.font.Font(None, 50)
     frontinfo = pygame.font.Font(None, 40)
-    txtmain = fontmain.render("Welcome to Tetris", True, BLACK)
-    txtstart = frontinfo.render("start game", True, BLACK)
-    txtscore = frontinfo.render("score board", True, BLACK)
-    txtsetting = frontinfo.render("settings", True, BLACK)
-    txtend = frontinfo.render("exit", True, BLACK)
+    txtmain = fontmain.render("Welcome to Tetris", True, color["BLACK"])
+    txtstart = frontinfo.render("start game", True, color["BLACK"])
+    txtscore = frontinfo.render("score board", True, color["BLACK"])
+    txtsetting = frontinfo.render("settings", True, color["BLACK"])
+    txtend = frontinfo.render("exit", True, color["BLACK"])
     self.blit(txtmain, (200, 50))
     self.blit(txtstart, (30, 400))
     self.blit(txtscore, (30, 430))
@@ -40,17 +49,25 @@ def drawmain(self, index, check):
     self.blit(txtend, (30, 490))
     if index == 0 and check:
         pygame.draw.polygon(
-            self, BLACK, [[180, 415], [180+10*3**0.5, 425], [180+10*3**0.5, 405]])
+            self, color["BLACK"], [[180, 415], [180+10*3**0.5, 425], [180+10*3**0.5, 405]])
     elif index == 1 and check:
         pygame.draw.polygon(
-            self, BLACK, [[198, 445], [198+10*3**0.5, 455], [198+10*3**0.5, 435]])
+            self, color["BLACK"], [[198, 445], [198+10*3**0.5, 455], [198+10*3**0.5, 435]])
     elif index == 2 and check:
         pygame.draw.polygon(
-            self, BLACK, [[153, 475], [153+10*3**0.5, 485], [153+10*3**0.5, 465]])
+            self, color["BLACK"], [[153, 475], [153+10*3**0.5, 485], [153+10*3**0.5, 465]])
     elif index == 3 and check:
         pygame.draw.polygon(
-            self, BLACK, [[90, 505], [90+10*3**0.5, 515], [90+10*3**0.5, 495]])
+            self, color["BLACK"], [[90, 505], [90+10*3**0.5, 515], [90+10*3**0.5, 495]])
     pygame.display.flip()
+
+
+def drawtable(self):
+    x = 1
+
+
+def startgame(self):
+    drawtable(self)
 
 
 def initgame():
@@ -73,13 +90,15 @@ def initgame():
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_RETURN:
                     if idx == 0:
-                        screen.fill(YELLOW)
-                        pygame.display.flip()
+                        startgame(screen)
+                        check = 1
+                        start_time = pygame.time.get_ticks()
+                        drawmain(screen, idx, check)
                     elif idx == 1:
-                        screen.fill(PINK)
+                        screen.fill(color["PINK"])
                         pygame.display.flip()
                     elif idx == 2:
-                        screen.fill(LIGHTGREEN)
+                        screen.fill(color["LIGHTGREEN"])
                         pygame.display.flip()
                     elif idx == 3:
                         run = False
